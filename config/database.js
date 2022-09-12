b9308056389c129bb390f0816d3f011483de4ffa
Natã Araújo <35064731+natanarau@users.noreply.github.com>
2022-09-12 17:01:44 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'cluster0.t3rgnkr.mongodb.net'),
+        host: env('DATABASE_HOST', ''),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'cockpit-atlas'),
-        username: env('DATABASE_USERNAME', 'usercockpit'),
-        password: env('DATABASE_PASSWORD', 'Z6CUYJCyw9TRirc'),
+        database: env('DATABASE_NAME', ''),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
